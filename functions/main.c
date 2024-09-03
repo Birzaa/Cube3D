8a0b282fe2fa4850e05c9c1c35748ecebc6ca57e
Aurelien Bougrainville <abougrai@made-f0Dr9s19.clusters.42paris.fr>
2024-09-03 17:03:23 +0200

    capart

--- functions/main.c
+++ functions/main.c
@@ -6,20 +6,32 @@
 /*   By: abougrai <abougrai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 22:09:04 by abougrai          #+#    #+#             */
-/*   Updated: 2024/08/23 22:05:15 by abougrai         ###   ########.fr       */
+/*   Updated: 2024/09/03 16:56:10 by abougrai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+void	check_file(t_data *data)
+{
+	data->fd = open(data->file, __O_DIRECTORY);
+	if (data->fd != -1)
+	{
+		close(data->fd);
+		exit_prog(data, "Error file\n", NULL);
+	}
+}
+
 int	main(int ac, char **av)
 {
+	(void)av;
 	t_data	data;
 
 	if (ac == 2)
 	{
 		valid_extension(av[1]);
 		init(&data, av[1]);
+		check_file(&data);
 		parsing(&data);
 		init_window(&data);
 		init_game(&data);
